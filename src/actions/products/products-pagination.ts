9ce4a257e1d24f9dfad20f3363f97a2d63be5844
Vicente Scópise <vscopise@gmail.com>
2025-08-07 08:36:23 -0300

    pp 10

--- src/actions/products/products-pagination.ts
+++ src/actions/products/products-pagination.ts
@@ -2,24 +2,16 @@
 
 import { Product } from "@/interfaces";
 
-/* interface PaginationOptions {
-  page?: number;
-  perPage?: number;
-} */
-
 interface ProductsResponse {
   products: Product[];
   totalPages: number;
-  //currentPage: number;
-} 
+}
 
 export async function getProducts(
   page: number = 1,
   perPage: number = 10
-): Promise<ProductsResponse | undefined> {
-  //try {
+): Promise<ProductsResponse> {
   const url = `${process.env.WC_API_URL}/products?page=${page}&per_page=${perPage}`;
-  //const url = new URL(`${process.env.WC_API_URL}/products`);
   const consumerKey = process.env.WC_CONSUMER_KEY!;
   const consumerSecret = process.env.WC_CONSUMER_SECRET!;
   const credentials = Buffer.from(`${consumerKey}:${consumerSecret}`).toString(
@@ -29,9 +21,7 @@ export async function getProducts(
   if (isNaN(Number(page))) page = 1;
   if (page < 1) page = 1;
 
-  console.log(`${url}/?page=${page}&per_page=${perPage}`);
   const response = await fetch(`${url}/?page=${page}&per_page=${perPage}`, {
-    //method: "GET",
     headers: {
       Authorization: `Basic ${credentials}`,
       "Content-Type": "application/json",
@@ -39,26 +29,13 @@ export async function getProducts(
   });
 
   if (!response.ok) {
-    //throw new Error(`WooCommerce API error ${response.status}`);
-    return {products:[], totalPages: 0};
+    return { products: [], totalPages: 0 };
   }
   const products = await response.json();
   const totalPages = Number(response.headers.get("x-wp-totalpages") || "0");
-  //const products = await response.json();
-
-  console.log({ products, totalPages });
 
   return {
     products: products,
     totalPages: totalPages,
-    //currentPage: page,
   };
-  /* } catch (err) {
-    console.error("WooCommerce fetch error:", err);
-    return {
-      products: [],
-      totalPages: 0,
-      //currentPage: 0,
-    };
-  } */
 }
