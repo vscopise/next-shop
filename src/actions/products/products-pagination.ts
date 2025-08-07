8a3601800563547ef20f123b5562b41db3d9d69a
Vicente Scópise <vscopise@gmail.com>
2025-08-07 08:29:49 -0300

    pp 8

--- src/actions/products/products-pagination.ts
+++ src/actions/products/products-pagination.ts
@@ -16,48 +16,49 @@ interface ProductsResponse {
 export async function getProducts(
   page: number = 1,
   perPage: number = 10
-): Promise<ProductsResponse> {
-  try {
-    //const url = `${process.env.WC_API_URL}/products`;
-    const url = new URL(`${process.env.WC_API_URL}/products`);
-    const consumerKey = process.env.WC_CONSUMER_KEY!;
-    const consumerSecret = process.env.WC_CONSUMER_SECRET!;
-    const credentials = Buffer.from(
-      `${consumerKey}:${consumerSecret}`
-    ).toString("base64");
+): Promise<ProductsResponse | undefined> {
+  //try {
+  const url = `${process.env.WC_API_URL}/products?page=${page}&per_page=${perPage}`;
+  //const url = new URL(`${process.env.WC_API_URL}/products`);
+  const consumerKey = process.env.WC_CONSUMER_KEY!;
+  const consumerSecret = process.env.WC_CONSUMER_SECRET!;
+  const credentials = Buffer.from(`${consumerKey}:${consumerSecret}`).toString(
+    "base64"
+  );
 
-    if (isNaN(Number(page))) page = 1;
-    if (page < 1) page = 1;
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
 
-    console.log(`${url}/?page=${page}&per_page=${perPage}`);
-    const response = await fetch(`${url}/?page=${page}&per_page=${perPage}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Basic ${credentials}`,
-        "Content-Type": "application/json",
-      },
-    });
+  console.log(`${url}/?page=${page}&per_page=${perPage}`);
+  const response = await fetch(`${url}/?page=${page}&per_page=${perPage}`, {
+    //method: "GET",
+    headers: {
+      Authorization: `Basic ${credentials}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (!response.ok) {
-      throw new Error(`WooCommerce API error ${response.status}`);
-    }
+  if (!response.ok) {
+    //throw new Error(`WooCommerce API error ${response.status}`);
+    return undefined;
+  }
+  const products = await response.json();
+  const totalPages = Number(response.headers.get("x-wp-totalpages") || "0");
+  //const products = await response.json();
 
-    const totalPages = Number(response.headers.get("x-wp-totalpages") || "0");
-    const products = await response.json();
+  console.log({ products, totalPages });
 
-    console.log({ products, totalPages });
-
-    return {
-      products: products,
-      totalPages: totalPages,
-      //currentPage: page,
-    };
-  } catch (err) {
+  return {
+    products: products,
+    totalPages: totalPages,
+    //currentPage: page,
+  };
+  /* } catch (err) {
     console.error("WooCommerce fetch error:", err);
     return {
       products: [],
       totalPages: 0,
       //currentPage: 0,
     };
-  }
+  } */
 }
